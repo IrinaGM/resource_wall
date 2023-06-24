5afa98f47b9ea419dc5cdf4b6bf9e5203e5904e9
IrinaGM <irina.golionko@gmail.com>
2023-06-24 17:55:46 +0000

    add seeds for resources and comments

---
+++ db/seeds/03_resources.sql
@@ -0,0 +1,23 @@
+-- resources table seeds here
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (1, 'https://www.codecademy.com/learn/introduction-to-javascript', 'Introduction to JavaScript', 'Learn how to use JavaScript — a powerful and flexible programming language for adding website interactivity.', 1, 1);
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (2, 'https://nodejs.dev/en/learn/', 'Node.js Tutorial', 'Getting started guide to Node.js, the server-side JavaScript runtime environment.', 1, 2);
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (3, 'https://guides.rubyonrails.org/getting_started.html', 'Getting Started with Rails', 'guide covers getting up and running with Ruby on Rails', 2, 3);
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (4, 'https://developer.mozilla.org/en-US/docs/Learn/HTML', 'Structuring the web with HTML', 'How to structure website with HTML', 2, 4);
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (5, 'https://sass-lang.com/guide', 'SASS Guide', 'SASS Basics buide and how to start using it.', 3, 5);
+
+INSERT INTO resources (id, url, title, description, user_id, topic_id)
+VALUES (5, 'https://www.codecademy.com/learn/react-101', 'React 101', 'Build powerful interactive applications with one of the most popular JavaScript libraries.', 4, 6);
+
+
+
+
