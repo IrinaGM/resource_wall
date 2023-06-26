eaa90ba38abac7e0d96e704a5b0cb6e1ecec969b
TinyMito <k.ip@me.com>
2023-06-25 22:57:32 -0400

    Revert "Merge branch 'main' into initialFrontEnd"
    This reverts commit dffeb4d04a6f83d2f89928154bcb068895b237e8, reversing
    changes made to d91fce8fc38a1505e9d6eef539e0a5c1808da607.

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -1,55 +1,28 @@
 const db = require('../connection');
 
 const getAllResources = () => {
-  // define query
-  const queryString = 'SELECT * FROM resources;';
-
-  // query the db
-  return db.query(queryString)
+  return db.query('SELECT * FROM users;')
     .then(data => {
       return data.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
     });
 };
-const getResourcesByUserId = (user_id) => {
-  // define query
-  const queryString = `SELECT * FROM resources WHERE user_id = $1;`;
-
-  //define values
-  const values = [user_id];
-
-  // query the db
-  return db.query(queryString, values)
+const getResourcesByUserId = (user_id, limit = 10) => {
+  return db.query(`SELECT * FROM users WHERE id = $1 LIMIT $2;`, [user_id,limit])
     .then(data => {
       return data.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
     });
 };
 const getSpecificResourceByUserId= (resource_id, user_id) =>{
-    // define query
-    const queryString = `SELECT * FROM resources WHERE id = $1 AND user_id = $2;`;
-
-    //define values
-    const values = [resource_id, user_id];
-
-  // query the db
-  return db.query(queryString, values)
+  return db.query(`SELECT * FROM users WHERE id = $1
+ /* AND user_id = $2*/
+  ;`, [resource_id])
+    //, user_id])
     .then(data => {
       return data.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
     });
 
 };
-
-module.exports =
-  {
-    getAllResources,
-    getResourcesByUserId,
-    getSpecificResourceByUserId
-  };
+module.exports = { getAllResources,
+                  getResourcesByUserId,
+                  getSpecificResourceByUserId
+                 };
