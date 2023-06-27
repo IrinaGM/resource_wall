7ba49412e16f0189d7995fc78a4fcb717b2e5f58
Kev <75095713+TinyMito@users.noreply.github.com>
2023-06-27 15:39:00 -0400

    Fix up the routing

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
+const defaultRoutes = require('./routes/routes');
 const userApiRoutes = require('./routes/users-api');
 const resourcesApiRoutes = require('./routes/resources-api');
 const loginRoutes = require('./routes/login');
@@ -45,11 +46,11 @@ const logoutRoutes = require('./routes/logout');
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/', resourcesApiRoutes);
-app.use('/myresources', resourcesApiRoutes);
+app.use('/', defaultRoutes);
+app.use('/api/resources', resourcesApiRoutes);
+app.use('/api/users', userApiRoutes);
 app.use('/login',loginRoutes);
 app.use('/logout',logoutRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
