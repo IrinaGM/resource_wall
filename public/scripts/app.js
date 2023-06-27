a1386294d35b9bf55df95ddfab1419539ed5ae98
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-06-27 19:11:19 -0400

    draft of search functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,20 @@
 // Client facing scripts here
 $(() => {
-
+   function getAllResources() {
+    let url = "/api/resources";    
+    return $.ajax({
+      url,
+    });
+  }
+  $("#searchForm").on('submit', function(event) {
+    event.preventDefault();
+    const data = $("#searchInput").value;
+    alert(data);
+    const resources =getAllResources();
+    alert(resources);
+    if(resources){
+      let filteredResources;     
+    }
+    
+  });
 });
