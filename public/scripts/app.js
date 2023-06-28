fa1303669652f8ba34b7098a11f3ac3de98c1c72
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-06-27 20:28:18 -0400

    intial draft for search

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,12 +9,22 @@ $(() => {
   $("#searchForm").on('submit', function(event) {
     event.preventDefault();
     const data = $("#searchInput").value;
-    alert(data);
-    const resources =getAllResources();
-    alert(resources);
-    if(resources){
-      let filteredResources;     
-    }
+    console.log(data);
+    const resources =getAllResources();//use promises
+    $("#content").empty();
+    resources.array.forEach(element => {
+      /*
+      <a href="<%= item.url %>" target="_blank">
+    <item>
+      <img alt="<%= item.title %>" src="./images/200x200-JS.png"/>
+      <span><%= item.title %></span>
+      <topic><%= item.topic_id %></topic>
+    </item>
+    </a>*/
+});
+
+  
+   
     
-  });
+ 
 });
