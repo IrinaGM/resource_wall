16ae894bb62de3e14f81e9321b2935ad4288deed
Kev <75095713+TinyMito@users.noreply.github.com>
2023-06-28 18:34:34 -0400

    Updated Seeds for more ratings

--- db/seeds/05_ratings.sql
+++ db/seeds/05_ratings.sql
@@ -1,18 +1,71 @@
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 1, 2);
 
 INSERT INTO ratings (rate, isLike, user_id, resource_id)
-VALUES (4, true, 4, 1);
-
-INSERT INTO ratings (rate, isLike, user_id, resource_id)
-VALUES (5, true, 3, 2);
-
-INSERT INTO ratings (rate, isLike, user_id, resource_id)
-VALUES (3, false, 2, 3);
+VALUES (3, false, 1, 3);
 
 INSERT INTO ratings (rate, isLike, user_id, resource_id)
 VALUES (2, false, 1, 4);
 
 INSERT INTO ratings (rate, isLike, user_id, resource_id)
-VALUES (5, true, 4, 5);
+VALUES (5, true, 1, 5);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, true, 1, 6);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (4, true, 2, 1);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, false, 2, 3);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (2, false, 2, 4);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 2, 5);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (2, true, 2, 6);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, false, 3, 3);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, false, 3, 4);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, true, 3, 5);
 
 INSERT INTO ratings (rate, isLike, user_id, resource_id)
 VALUES (5, true, 3, 6);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (4, true, 4, 1);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (2, true, 4, 2);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (2, false, 4, 4);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 4, 5);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, true, 5, 1);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 5, 2);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (3, false, 5, 3);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 5, 6);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (4, true, 6, 1);
+
+INSERT INTO ratings (rate, isLike, user_id, resource_id)
+VALUES (5, true, 6, 2);
