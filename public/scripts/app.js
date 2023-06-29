5653e5314ca74651a11ce864036bc62a9207af23
Kev <75095713+TinyMito@users.noreply.github.com>
2023-06-29 09:21:51 -0400

    Revert the changes for now same as main.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,50 +5,41 @@ $(() => {
     if (Array.isArray(resources)) {
         $("#search-container").empty();
         resources.forEach((item) => {
-        $res= $(`
-        <a class="itemLink" href="/resource?id=${ item.id }">
-          <item>
-            <img alt="${ item.title }" src="./images/200x200-JS.png"/>
-            <span class="topic">${ item.topic_name}</span>
-            <span class="title">${ item.title }</span>
-          </item>
-        </a>
-        `);
+        $res= $(`<a class="itemLink" href="/resource?id=${ item.id }">
+        <item>
+          <img alt="${ item.title }" src="./images/200x200-JS.png"/>
+          <span class="topic">${ item.topic_name}</span>
+          <span class="title">${ item.title }</span>
+        </item>
+        </a>`);
         $("#search-container").append($res);
       });
     }
   };
-
-  let form = $("#searchForm");
-
-  form.submit((event) => {
-    event.preventDefault(); // prevents default behavior of the event
-
-    let textArea = $("#searchInput");
-
-    if (!textArea.val() || textArea.val().length === 0) { // if search data is empty
-      $.ajax("/", { method: "GET" });
-    }
-
-    const url = form.attr("action");
-    const search_text = textArea.val().toUpperCase();
-
-    $.ajax(url, { method: "GET" })
-      .then(function (all_resources) {
+    let form = $("#searchForm");
+    form.submit((event) => {
+      event.preventDefault(); // prevents default behavior of the event
+      let textArea = $("#searchInput");
+      if (!textArea.val() || textArea.val().length === 0) { // if search data is empty
+        $.ajax("/", { method: "GET" });
+      }
+      const url = form.attr("action");
+      const search_text= textArea.val().toUpperCase();
+      $.ajax(url, { method: "GET" }).then(function (all_resources) {
         //perform search
-        if (all_resources) {
-          let output = all_resources.resources;
-          const searchedResources = output.filter(res => res.title.toUpperCase().includes(search_text) || res.description.toUpperCase().includes(search_text));
-          renderSearchedResources(searchedResources);
+        if(all_resources){
+        let output= all_resources.resources;
+        const searchedResources=  output.filter(res => res.title.toUpperCase().includes(search_text) || res.description.toUpperCase().includes(search_text));
+        renderSearchedResources(searchedResources);
         }
       })
       .catch(err => {
-        console.log(`error: ${err.message}`);
+        console.log(`error: ${err.message} `);
+      });
     });
   });
-});
 
-// Navigate back to the previous page
-function goBack() {
-  history.back();
-}
+  // Navigate back to the previous page
+  function goBack() {
+    history.back();
+  }
